222e1513f063a057d656f169bfa509acfadaaf50
tusba <11007086+tusba@users.noreply.github.com>
2022-05-13 11:34:31 +0600

    add CLI controller

---
+++ components/cli/CliController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace components\cli;
+
+use components\logger\InitLoggerTrait;
+use components\logger\LoggerInterface;
+use components\services\DataFilterService;
+use Exception;
+
+class CliController
+{
+    const COUNT_BY_PRICE_RANGE = 'count_by_price_range';
+    const COUNT_BY_VENDOR_ID = 'count_by_vendor_id';
+
+    private LoggerInterface $logger;
+    private string $command;
+    private array $options;
+
+    use InitLoggerTrait;
+
+    public function __construct()
+    {
+        $this->initLogger();
+    }
+
+    public function run(array $argv): mixed
+    {
+        $this->resolve($argv);
+        $this->validate();
+        $this->logger->log(['cmd' => $this->command, 'args' => $this->options]);
+
+        $data = [];
+        $this->logger->log(['data' => ['total' => count($data)]]);
+
+        $dataFilterService = new DataFilterService($data);
+        switch ($this->command) {
+            case self::COUNT_BY_PRICE_RANGE:
+                $filteredData = $dataFilterService->filterByPriceRange(...$this->options);
+                break;
+            case self::COUNT_BY_VENDOR_ID:
+                $filteredData = $dataFilterService->filterByVendorId(...$this->options);
+                break;
+            default:
+                throw new Exception("Not implemented for [{$this->command}]");
+        }
+        $this->logger->log(['data' => ['result' => $filteredData]]);
+
+        return count($filteredData);
+    }
+
+    private function api(): array
+    {
+        return [
+            self::COUNT_BY_PRICE_RANGE => ['from', 'to'],
+            self::COUNT_BY_VENDOR_ID => ['id'],
+        ];
+    }
+
+    private function resolve(array $argv): void
+    {
+        $argc = count($argv);
+
+        if ($argc < 2) {
+            throw new Exception('Too few arguments');
+        }
+
+        $this->command = $argv[1];
+        $this->options = array_slice($argv, 2);
+    }
+
+    private function validate(): void
+    {
+        $api = $this->api();
+
+        if (!isset($api[$this->command])) {
+            throw new Exception("Unrecognized command: `{$this->command}`");
+        }
+
+        $argc = count($api[$this->command]);
+        if ($argc > count($this->options)) {
+            throw new Exception("`{$this->command}` command accepts {$argc} argument(-s)");
+        }
+    }
+}
--- run.php
+++ run.php
@@ -1,17 +1,29 @@
 <?php
 
 const DS = DIRECTORY_SEPARATOR;
+const EOL = PHP_EOL;
 
 spl_autoload_extensions('.php');
 spl_autoload_register();
 
-use \components\logger\FileSystemLogger;
+use components\cli\CliController;
+use components\logger\FileSystemLogger;
 
 $logDir = __DIR__ . DS . 'log';
 $logFileName = date('Y-m-d') . '.txt';
 try {
     $logger = new FileSystemLogger($logDir . DS . $logFileName);
 } catch (Exception $e) {
-    printf('Error on logging: "%s"' . PHP_EOL, $e->getMessage());
+    printf('Error on logging: "%s"' . EOL, $e->getMessage());
     exit(1);
 }
+
+$cli = new CliController();
+try {
+    $result = $cli->run($argv);
+} catch (Exception $e) {
+    printf('Error on resolving CLI arguments: "%s"' . EOL, $e->getMessage());
+    exit(1);
+}
+
+printf('Result is %d' . EOL, $result);
